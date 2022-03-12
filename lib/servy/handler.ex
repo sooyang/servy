49dc3377672b81ae497ef48d9bf4afeff3bae37f
Soo Yang, Tan <sooyang.code@gmail.com>
2022-03-12 12:44:06 +0800

    exercises

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -76,9 +76,23 @@ defmodule Servy.Handler do
     %{conv | status: 200, resp_body: "Bear #{id}"}
   end
 
+  def route(%{method: "GET", path: "/pages/" <> file} = conv) do
+    Path.expand("../../pages", __DIR__)
+    |> Path.join(file <> ".html")
+    |> File.read
+    |> handle_file(conv)
+  end
+
   def route(%{method: "GET", path: "/about"} = conv) do
     Path.expand("../../pages", __DIR__)
     |> Path.join("about.html")
+    |> File.read
+    |> handle_file(conv)
+  end
+
+  def route(%{method: "GET", path: "/bears/new"}  = conv) do
+    Path.expand("../../pages", __DIR__)
+    |> Path.join("form.html")
     |> File.read
     |> handle_file(conv)
   end
@@ -229,6 +243,18 @@ IO.puts response
 
 request = """
 GET /about HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /bears/new HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
